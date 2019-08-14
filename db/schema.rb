e11419386cd3e7d74bfd44a7db5a4c5f27b06d37
Ritesh Kapoor <ritesh@workoidz.com>
2019-08-14 19:58:50 +0530

    done till 09

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20190814092001) do
     t.string   "title"
     t.text     "description"
     t.boolean  "completed"
-    t.integer  "todo_lists_id"
+    t.integer  "todo_list_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+TodoItem.destroy_all
+
+User.create!(
+		[
+			{username: 'Fiorina', password_digest: 'abc123'},
+			{username: 'Trump', password_digest: 'abc123'},
+			{username: 'Carson', password_digest: 'abc123'},
+			{username: 'Clinton', password_digest: 'abce123'}
+		]
+
+	)
+
+Profile.create!(
+		[
+			{gender: 'female', birth_year: 1954, first_name: 'Carly', last_name: 'Fiorina', user_id: User.find_by(username: 'Fiorina').id},
+			{gender: 'male', birth_year: 1946, first_name: 'Donald', last_name: 'Trump', user_id: User.find_by(username: 'Trump').id},
+			{gender: 'male', birth_year: 1951, first_name: 'Ben', last_name: 'Carson', user_id: User.find_by(username: 'Carson').id},
+			{gender: 'female', birth_year: 1947, first_name: 'Hillary', last_name: 'Clinton', user_id: User.find_by(username: 'Clinton').id}
+		]
+
+	)
+
+TodoList.create!(
+		[
+			{list_name: 'Month Plan', list_due_date: Date.today + 1.year, user_id: User.find_by(username: 'Fiorina').id},
+			{list_name: 'Last Sunday', list_due_date: Date.today + 1.year, user_id: User.find_by(username: 'Trump').id},
+			{list_name: 'ToDo Day', list_due_date: Date.today + 1.year, user_id: User.find_by(username: 'Carson').id},
+			{list_name: 'Last Monday', list_due_date: Date.today + 1.year, user_id: User.find_by(username: 'Clinton').id}
+			
+		]
+
+	)
+
+
+	4.times do |k|
+		5.times do |i|
+
+			TodoItem.create!(
+					{due_date: Date.today + 1.year, title: "List item #{i}", description: "this is #{i} discription", completed: false, todo_list_id: TodoList.first.id+k}
+				)
+
+		end
+	end
+
+
+
+
+
+
+
