3abf399b9a87b7c04147177a46930ff6b32e1bae
Ritesh Kapoor <ritesh@workoidz.com>
2019-08-15 13:26:06 +0530

    rq12 completed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,37 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :first_last_both_not_null, :either_male_or_female, :sue_cannot_be_male
+
+  def sue_cannot_be_male
+
+  	if (first_name == "Sue" and gender == "male")
+  		errors.add(:first_name," Sue Cannot be male ")
+  	end
+  	
+  end
+
+
+  def either_male_or_female
+
+  	if (gender != "male" and gender != "female")
+  		errors.add(:gender,"Either male or female")
+	end
+
+  end
+
+
+
+  def first_last_both_not_null
+
+  	if (first_name.nil?  and last_name.nil?)
+  		errors.add(:first_name," First Name ANd Last Name, both cannot be emptied ")
+  	end
+  	
+  end
+
+
+
+
 end
+
