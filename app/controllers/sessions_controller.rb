e3bd2e2824e6151fb916a8a5953afde050a02c8d
Ritesh Kapoor <ritesh@workoidz.com>
2019-08-18 06:40:25 +0530

    10c done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+
+  	username = User.find_by(username: params[:user][:username])
+  	password = params[:user][:password]
+
+  	if username and username.authenticate(password)
+  		session[:user_id] = username.id 
+  		redirect_to root_path, notice: "Logged in successfully"
+  	else
+  		redirect_to login_path, notice: "Invail Username / Password"
+  	end
+
   end
 
   def destroy
